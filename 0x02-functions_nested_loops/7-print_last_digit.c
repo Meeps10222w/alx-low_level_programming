a30aac9ec2e6401d0f08432f7b5e9276596f1924
Kenny <you@example.com>
2022-07-06 11:09:04 -0700

    Hot fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,8 +7,17 @@
 */
 int print_last_digit(int n)
 {
+int nv;
 	if (n < 0)
-	n *= -1;
-       	_putchar('0' + (n % 10));
-	return (n % 10);
+	{
+	nv = -1 * (n % 10);
+	_putchar(nv + '0');
+	return (nv);
+	}
+	else
+	{
+	nv = n % 10;
+	_putchar(nv + '0');
+	return (nv);
+	}
 }
