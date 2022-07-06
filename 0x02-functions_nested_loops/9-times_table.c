22fced064aaa929ef5b332e0a8367601c120ef8f
Kenny <you@example.com>
2022-07-06 11:23:23 -0700

    Hot advanced fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,9 +5,10 @@
 */
 void times_table(void)
 {
-	int row;
-	int column;
-	int product;
+int row;
+int column;
+int product;
+
 	for (row = 0; row <= 9; row++)
 	{
 	for (column = 0; column <= 9; column++)
