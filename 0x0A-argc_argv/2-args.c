60340022b19a81d7a7d5b408132bce1b6098f6c6
Kenny <you@example.com>
2022-07-22 12:09:29 -0700

    Advanced fix

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -6,11 +6,13 @@
 * @argv: array that contains the program command line arguments.
 * Return: 0 - success.
 */
+
 int main(int argc, char *argv[])
 {
 	int i;
-	
-	for (i = 0; i < argc; i++)
-		printf("%s\n", argv[i]);
+
+	while (i < argc)
+		printf("%s\n", argv[i++]);
+
 	return (0);
 }
